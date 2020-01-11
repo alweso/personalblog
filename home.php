c09a4af9ab6419ea52e367aae7bc691242ea8ef5
alweso <alweso@gmail.com>
2020-01-11 16:46:38 +0100

    scrolling load more and sorting posts work, but too many calls

--- home.php
+++ home.php
@@ -15,28 +15,25 @@ get_header(); ?>
 		<?php
 		the_archive_title( '<h1 class="page-title mt-4 mb-4">', '</h1>' );
 		?> home.php
+		<form id="misha_filters" action="#">
+ 
+	<label for="misha_order_by">Order</label>
+	<select name="misha_order_by" id="misha_order_by">
+		<option value="date-DESC">Date ↓</option><!-- I will explode these values by "-" symbol later -->
+		<option value="date-ASC">Date ↑</option>
+		<option value="comment_count-DESC">Comments ↓</option>
+		<option value="comment_count-ASC">Comments ↑</option>
+	</select>
+	<!-- required hidden field for admin-ajax.php -->
+	<input type="hidden" name="action" value="mishafilter" />
+</form>
+<div id="response"></div>
 	</header><!-- .page-header -->
 	
 <!-- 	<h2><?php the_posts_pagination(); ?>dsfdasfdsafdsafsa</h2>  -->
-	<?php if ( have_posts() ) : ?>
-		<div class="row allposts row-eq-height">
-			<?php
-			while ( have_posts() ) :
-				the_post();
-				?>
-				<div class="col-xs-12 col-sm-4">
-					<?php get_template_part( 'template-parts/archive-post/content', get_post_format() ); ?>
-				</div>
-				<?php
-// End the loop.
-			endwhile;
-			?>
-		</div>
-		<?php
-	else :
-		get_template_part( 'template-parts/archive-post/content', 'none' ); 
-	endif;
-	?>
+<div id="misha_posts_wrap" class="row">
+	<!-- Posts will be here -->
+</div>
 	<?php
 if (  $wp_query->max_num_pages > 1 ) :
 	echo '<div id="misha_loadmore">More posts</div>'; // you can use <a> as well
